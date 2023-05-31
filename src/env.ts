0af0418c0f8cb04eb1b3ce2dc56344f68ed8c2fd
crissdev <criss.trifan@gmail.com>
2023-05-31 08:48:08 +0300

    defaults endpoints to known values

--- src/env.ts
+++ src/env.ts
@@ -6,7 +6,11 @@ const schema = z.object({
   BNR_FX_RATES_OF_YEAR_URL: z.string().url(),
 });
 
-const parseResult = schema.safeParse(process.env);
+const parseResult = schema.safeParse({
+  BNR_FX_RATES_MOST_RECENT_URL: "https://www.bnr.ro/nbrfxrates.xml",
+  BNR_FX_RATES_OF_YEAR_URL: "https://bnr.ro/files/xml/years/nbrfxrates{year}.xml",
+  ...process.env,
+});
 
 if (!parseResult.success) {
   throw new Error("Environment variables are not defined.");
