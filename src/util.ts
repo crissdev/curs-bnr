ca1df672e570f68811a339108b6a9d1582f29a52
crissdev <criss.trifan@gmail.com>
2023-06-13 11:41:47 +0300

    fetch exchange rates for a specific date

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,11 @@
 import env from "@/env";
 
-export function getExchangeRatesUrlForYear(year: number) {
+const ISO_DATE_RE = /^\d{4}-\d{2}-\d{2}$/;
+
+export function getYearlyExchangeRatesUrl(year: number) {
   return env.BNR_FX_RATES_OF_YEAR_URL.replace("{year}", year.toString());
+}
+
+export function isValidDateString(date: string) {
+  return ISO_DATE_RE.test(date);
 }
