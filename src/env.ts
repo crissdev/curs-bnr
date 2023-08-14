dfbf658b3173b7aa8c1e6b5dfc74a0ef57114fbd
crissdev <criss.trifan@gmail.com>
2023-08-14 12:26:52 +0200

    replace zod with valibot

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,11 @@
-import z from "zod";
+import { object, safeParse, string, url } from "valibot";
 
-const schema = z.object({
-  BNR_FX_RATES_MOST_RECENT_URL: z.string().url(),
-  BNR_FX_RATES_OF_YEAR_URL: z.string().url(),
+const schema = object({
+  BNR_FX_RATES_MOST_RECENT_URL: string([url()]),
+  BNR_FX_RATES_OF_YEAR_URL: string([url()]),
 });
 
-const parseResult = schema.safeParse({
+const parseResult = safeParse(schema, {
   BNR_FX_RATES_MOST_RECENT_URL: "https://www.bnr.ro/nbrfxrates.xml",
   BNR_FX_RATES_OF_YEAR_URL: "https://bnr.ro/files/xml/years/nbrfxrates{year}.xml",
   ...process.env,
